f658233dd4ca65fdcebb9b17b1b6cb05457cf972
kohli-rahman <139750880+kohli-rahman@users.noreply.github.com>
2025-04-18 20:24:01 +0530

    Update Auth.tsx

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -187,7 +187,7 @@ useEffect(() => {
 
     try {
       const { error } = await supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: `${window.location.origin}/auth`,
+        redirectTo: `${window.location.origin}/reset-password`,
 });
 
       if (error) throw error;
